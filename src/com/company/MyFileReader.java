4727415583776337ba36ba7abf9bf4a54b1d87dd
ReinoudvanZoelen <f4n9yEBbdrIe5CTaRxdV>
2017-12-24 18:04:45 +0100

    Working filereader and writer

---
+++ src/com/company/MyFileReader.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyFileReader implements Runnable {
+
+    private String path;
+
+    public MyFileReader(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public void run() {
+        PrintLines(ExtractLines());
+    }
+
+    private List<String> ExtractLines() {
+        System.out.println("- Extracting file");
+        List<String> records = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                records.add(line);
+            }
+            reader.close();
+            return records;
+        } catch (Exception e) {
+            System.err.format("- Exception occurred trying to read '%s'.", path);
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void PrintLines(List<String> records) {
+        System.out.println("- Reading file");
+        for (String record : records) {
+            System.out.println(record);
+        }
+    }
+
+}
---
+++ src/com/company/MyWatchservice.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+
+import java.util.logging.*;
+
+public class MyWatchservice {
+    public static void main(String[] args) {
+        final WatchService watcher;
+        Path dir = Paths.get("C:\\temp");
+        WatchKey key;
+
+        try {
+            watcher = FileSystems.getDefault().newWatchService();
+            dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+
+            while (true) {
+                key = watcher.take();
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    WatchEvent<Path> ev = (WatchEvent<Path>) event;
+
+                    Path filename = ev.context();
+                    Path child = dir.resolve(filename);
+
+                    WatchEvent.Kind kind = ev.kind();
+                    if (kind == ENTRY_CREATE) {
+                        System.out.println(child + " created");
+                        new MyFileReader(dir.toAbsolutePath().toString() + filename.toString()).run();
+                    }
+                    else if (kind == ENTRY_DELETE) {
+                        System.out.println(child + " deleted");
+                    }
+                    else if (kind == ENTRY_MODIFY) {
+                        System.out.println(child + " modified");
+                        new MyFileReader(dir.toAbsolutePath().toString() + '\\' + filename.toString()).run();
+                    }
+                }
+                key.reset();
+            }
+
+        } catch (IOException | InterruptedException ex) {
+            Logger.getLogger(MyWatchservice.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
